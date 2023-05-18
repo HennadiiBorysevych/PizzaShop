aeca257fbdc12c6fe30e3ab7314dac85aa4d4f41
Hennadii Borysevych <genuch3@gmail.com>
2023-05-18 19:49:32 +0300

    feat: added modal & pizza list % some logic for arder form in modal window

--- src/app/components/Pizza.js
+++ src/app/components/Pizza.js
@@ -1,5 +1,94 @@
-const Pizza = () => {
-  return <div>Pizza</div>;
+"use client";
+import { useState } from "react";
+//images
+import Image from "next/image";
+//madal
+import Modal from "react-modal";
+//components
+import PizzaDetails from "./PizzaDetails";
+//icons
+import { IoCloseOutline } from "react-icons/io5";
+
+//bind modal to body
+Modal.setAppElement("body");
+
+const modalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+};
+
+const Pizza = ({ pizza }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="group py-2 px-4 xl:py-4 xl:px-2 rounded-x1">
+      <Image
+        onClick={openModal}
+        className="lg:hover:translate-y-3 transition-all dutation-300 mb-8 cursor-pointer"
+        width={270}
+        height={270}
+        src={pizza.image}
+        alt=""
+        priority={1}
+      />
+      <div
+        onClick={openModal}
+        className="text-x1 font-bold mb-3 capitalize cursor-pointer "
+      >
+        {pizza.name}
+      </div>
+      <div className="text-sm font-medium min-h-[60px] mb-6">
+        {pizza.description}
+      </div>
+      <div className="mb-6 flex items-center justify-between">
+        <div className="hidden lg:flex text-x1 font-semibold">
+          starts at {pizza.priceSm}
+        </div>
+        <button
+          onClick={openModal}
+          className="hidden lg:flex gradient text-white rounded-lg btn-sm font-semibold text-sm"
+        >
+          Choose
+        </button>
+        <button
+          onClick={openModal}
+          className="btn btn-sm gradient text-sm lg:hidden px-3"
+        >
+          starts at {pizza.priceSm}
+        </button>
+      </div>
+      {/* modal */}
+      {modalIsOpen && (
+        <Modal
+          isOpen={modalIsOpen}
+          style={modalStyles}
+          onRequestClose={closeModal}
+          contentLabel="Pizza Modal"
+          className="bg-white w-full h-full lg:max-w-[900px] lg:max-h-[600px] lg:rounded-[30px] lg:fixed lg:top-[50%] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] outline-none"
+        >
+          <div
+            onClick={closeModal}
+            className="absolute z-30 right-2 top-2 hover:scale-110 duration-200 cursor-pointer"
+          >
+            <IoCloseOutline className="text-4xl text-orange" />
+          </div>
+          <PizzaDetails
+            pizza={pizza}
+            modal={modalIsOpen}
+            setModal={setModalIsOpen}
+          />
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default Pizza;
--- src/app/components/PizzaDetails.js
+++ src/app/components/PizzaDetails.js
@@ -1,5 +1,106 @@
-const PizzaDetails = () => {
-  return <div>PizzaDetails</div>;
+import React, { useState, useEffect } from "react";
+
+import Image from "next/image";
+
+import SizeSelection from "./SizeSelection";
+import CrustSelection from "./CrustSelection";
+import Topping from "./Topping";
+
+const PizzaDetails = ({ pizza }) => {
+  const [size, setSize] = useState("medium");
+  const [crust, setCrust] = useState("traditional");
+  const [additionalTopping, setAdditionalTopping] = useState([]);
+  const [additionalToppingPrice, setAdditionalToppingPrice] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    size === "small"
+      ? setTotalPrice(
+          parseFloat(pizza.priceSm + additionalToppingPrice).toFixed(2)
+        )
+      : size === "medium"
+      ? setTotalPrice(
+          parseFloat(pizza.priceMd + additionalToppingPrice).toFixed(2)
+        )
+      : size === "large"
+      ? setTotalPrice(
+          parseFloat(pizza.priceLg + additionalToppingPrice).toFixed(2)
+        )
+      : null;
+  }, [
+    size,
+    pizza.priceSm,
+    pizza.priceMd,
+    pizza.priceLg,
+    additionalToppingPrice,
+  ]);
+
+  useEffect(() => {
+    if (additionalTopping.length > 0) {
+      const toppingPrice = additionalTopping.reduce((acc, curr) => {
+        return acc + curr.price;
+      }, 0);
+      setAdditionalToppingPrice(toppingPrice);
+    } else {
+      setAdditionalToppingPrice(0);
+    }
+  }, [additionalTopping]);
+
+  return (
+    <div className="flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8 ">
+      <div className="lg:flex-1 flex justify-center items-center">
+        <div className="max-w-[300px] lg:max-w-none mt-6  lg:mt-0">
+          <Image
+            className="mt-auto relative"
+            width={450}
+            height={450}
+            src={pizza.image}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className=" flex flex-col flex-1">
+        <div className="flex-1 p-2 text-center lg:text-left">
+          <div
+            className="flex-1 bg-white overflow-y-scroll h-[46vh] 
+          scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-white pr-2"
+          >
+            <div className="font-semibold">
+              <h2 className="capitalize text-3xl mb-1">{pizza.name}</h2>
+              <div className="mb-6 text-lg font-medium">
+                <span>
+                  {size === "small"
+                    ? "25 cm"
+                    : size === "medium"
+                    ? "30 cm"
+                    : size === "large"
+                    ? "35 cm"
+                    : null}
+                </span>
+                <span>, {crust} crust</span>
+              </div>
+            </div>
+            <SizeSelection pizza={pizza} size={size} setSize={setSize} />
+            <CrustSelection crust={crust} setCrust={setCrust} />
+            <h2 className="mb-4 text-xl font-semibold">Choose toppings</h2>
+            <ul className="flex flex-wrap gap-2">
+              {pizza.toppings?.map((topping, index) => (
+                <li key={index}>
+                  <Topping topping={topping} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="h-full flex items-center px-2 lg:items-end">
+          <button className="btn btn-lg gradient w-full flex justify-center gap-x-2">
+            <div>Add to cart for</div>
+            <div>${totalPrice}</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PizzaDetails;
